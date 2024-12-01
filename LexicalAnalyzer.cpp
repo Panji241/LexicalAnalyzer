55659a9ee79952632d4b20c4137b9fe7bdbba096
Panji241 <14290894+Panji241@users.noreply.github.com>
2024-12-01 18:17:49 +0200

    Finalized

--- LexicalAnalyzer.cpp
+++ LexicalAnalyzer.cpp
@@ -3,85 +3,89 @@
 #include <string.h>
 #include <ctype.h>
 
-union semantic_info {
-    char* string;
+typedef union {
+    char *string;
     int i;
-};
+}SEMANTIC_INFO;
 
-int yylex(FILE *filePointer, char *string);
+int yylex();
+
+FILE* yyin = NULL;
+char* yytext = NULL;
+SEMANTIC_INFO yyval;
 
 int main(void) {
-    FILE* yyin = NULL;
-    char* yytext = NULL;
-    
-    fopen_s(&yyin, "Text.txt", "r");
-    if (yyin == NULL) {
+    if ((fopen_s(&yyin, "text.txt", "r")) != NULL) {
 		printf("Error: File not found!\n");
-        exit(-1);
+        return -1;
     }
 
     if ((yytext = (char*)calloc(128, sizeof(char))) == NULL) {
         printf("Error: Memory not allocated!\n");
-        exit(-1);
+        return -1;
     }
 
     int token;
-    while ((token = yylex(yyin, yytext)) != 0) {
+    while ((token = yylex()) != 0) {
         if (token == 1) {
-            printf("Identifier: %s\n", yytext);
+			//yyval.string = yytext;
+			yyval.string = (char *) calloc(strlen(yytext) + 1, sizeof(char));
+			strcpy_s(yyval.string, sizeof(yyval.string), yytext);
+            printf("Identifier: %s\n", yyval.string);
         }
         else if (token == 2) {
-            printf("Number: %s\n", yytext);
+			yyval.i = atoi(yytext);
+            printf("Number: %d\n", yyval.i);
         }
         else {
             printf("Token: %d\n", token);
-            break;
         }
     }
 
     free(yytext);
-    if (yyin != NULL) fclose(yyin);
+    fclose(yyin);
     return 0;
 }
 
-int yylex(FILE *filePointer, char *string) {
+int yylex() {
     int c, textPointer = 0, state = 0;
 
     while(1) {
-        c = fgetc(filePointer);
+        //Fixed the issue with while returning -1 non stop
+        c = fgetc(yyin);
         switch (state) {
             case 0: {
                 if (isalpha(c)) {
                     state = 1;
-                    string[textPointer++] = c;
+                    yytext[textPointer++] = c;
                 }
                 else if (isdigit(c)) {
                     state = 2;
-                    string[textPointer++] = c;
+                    yytext[textPointer++] = c;
                 }
                 else if (c == EOF){
-                    return 0;
+					return 0; // Checking for c is EOF so it will stop the loop instead of cotinuing to spam -1
                 }
             }
             break;
             case 1: {
                 if (isalpha(c)) {
-                    string[textPointer++] = c;
+                    yytext[textPointer++] = c;
                 }
                 else {
-                    string[textPointer] = '\0';
-                    ungetc(c, filePointer);
+                    yytext[textPointer] = '\0';
+					ungetc(c, yyin); // Used ungetc to return the character back from EOF
                     return 1;
                 }
             }
             break;
             case 2: {
                 if (isdigit(c)) {
-                    string[textPointer++] = c;
+                    yytext[textPointer++] = c;
                 }
                 else {
-                    string[textPointer] = '\0';
-                    ungetc(c, filePointer);
+                    yytext[textPointer] = '\0';
+                    ungetc(c, yyin); // Used ungetc to return the character back from EOF
                     return 2;
                 }
             }
